afbaa5aa912e35a4085ee38beb8d10e820fab977
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2023-07-08 15:15:00 -0500

    eureka server

---
+++ EurekaServerApp/src/test/java/com/microservice/EurekaServerApp/EurekaServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.EurekaServerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
